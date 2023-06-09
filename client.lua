7e1172524d9b18ce38509af49bfea82531e7a131
HeyTax <77160060+HeyTax@users.noreply.github.com>
2023-06-09 07:56:03 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,11 +1,6 @@
 Citizen.CreateThread(function()
 
 
-AddTextEntry('eclipse', 'Mitshubishi Eclipse')
-
-AddTextEntry('supra2', 'Supra Clean')
-
-AddTextEntry('zx6r', 'kawasaki zx6r')
-
-AddTextEntry('s63arm', 'Gepanzerter Mercedes')
+AddTextEntry('test', 'test Car')
+    
 end)
